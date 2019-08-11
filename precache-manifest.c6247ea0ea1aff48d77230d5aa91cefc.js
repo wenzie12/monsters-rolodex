0b35ddd1e9c430c56ce61677741780fe36541a7a
wenzie <wenzie12sg@gmail.com>
2019-08-12 01:37:18 +0800

    Updates

---
+++ precache-manifest.c6247ea0ea1aff48d77230d5aa91cefc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ccae785077c7f6517475de5645e5c0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4dab37c9a21d090c33c8",
+    "url": "/monsters-rolodex/static/css/main.9b1a97d9.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "4dab37c9a21d090c33c8",
+    "url": "/monsters-rolodex/static/js/main.b5358709.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
